f2c0762b33de4a0fca45d6f15ad30d6641feb6ed
athapar9 <athapar@calpoly.edu>
2025-07-30 14:36:26 -0700

    fixing deployment issues

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/home',
-        permanent: true,
-      },
-    ];
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
